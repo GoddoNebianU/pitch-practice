b42817dfdf9ee606d24c7bb9025369a9479c142b
goddonebianu <goddonebianu@outlook.com>
2026-03-25 13:54:52 +0800

    Wed Mar 25 01:54:52 PM CST 2026

--- src/components/AccuracyForm.tsx
+++ src/components/AccuracyForm.tsx
@@ -8,7 +8,9 @@ export default function AccuracyForm({
     accuracy
 }: AccuracyFormProps) {
     return (
-        <div>正确率表格
+        <div
+        className="w-40"
+        >正确率表格
             {accuracy.map((acc, index) => {
                 return <div key={index} className="border">{CDEFGAB[index]}{"-->"}{acc}%</div>;
             })}
